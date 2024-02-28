7e9dcf2f005ac52ae9a6d42be8e998fd45de12f3
rhmunna143 <rhmunna19@gmail.com>
2024-02-29 03:07:40 +0600

    video section responsive done

--- src/components/video/VideoSection.jsx
+++ src/components/video/VideoSection.jsx
@@ -3,7 +3,7 @@ import video from "../../assets/video/no-sound.mp4";
 
 const VideoSection = () => {
     return (
-        <motion.div className="px-4 flex gap-8 mt-7 mb-20"
+        <motion.div className="px-4 flex flex-col md:flex-row gap-8 mt-7 mb-20"
             initial={{
                 y: -5
             }}
@@ -16,12 +16,12 @@ const VideoSection = () => {
                 duration: 0.5
             }}
         >
-            <div className="box bg-primaryBg pl-10 pr-16 py-14 font-medium text-4xl rounded-[33px] w-96 flex items-center">
+            <div className="box bg-primaryBg pl-10 pr-12 md:pr-16 py-14 font-medium text-4xl rounded-[33px] lg:w-96 md:w-1/2 w-3/4 flex items-center">
                 <p className="uppercase">Drink <br /> Directly <br /> from any <br /> source of <br /> Water</p>
             </div>
 
             <div className="video">
-                <video autoPlay loop muted className="rounded-[33px]" src={video} width={800}></video>
+                <video autoPlay loop muted className="w-96 lg:w-[800px] md:h-full md:w-full rounded-[33px] md:rounded-[33px] " src={video} width={800}></video>
             </div>
         </motion.div>
     );
