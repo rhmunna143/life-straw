1ef1736f0f960f22a4f9871b1208d736c3a23bef
rhmunna143 <rhmunna19@gmail.com>
2024-03-12 15:09:00 +0600

    header sticky done

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -15,63 +15,65 @@ const NavBar = () => {
     };
 
     return (
-        <Container>
-            <div className="bg-bg flex flex-col lg:flex-row flex-wrap justify-between items-center">
-                <div className="logo">
-                    <Link to="/">
-                        <img className="h-24 w-40" src={logo} alt="logo" />
-                    </Link>
-                </div>
-
-                <div className="menu flex flex-col md:flex-row gap-4 items-center">
-                    <div
-                        className={`menu-1 bg-primary flex flex-col gap-4 py-3 px-8 rounded-[33px] ${menuOpen ? 'block' : 'hidden md:block'
-                            } md:flex md:flex-row`}
-                    >
-                        <Link to="/shop" className="text-white font-medium no-underline">
-                            Shop
-                        </Link>
-                        <Link to="/technology" className="text-white font-medium no-underline">
-                            Technology
-                        </Link>
-                        <Link to="/support" className="text-white font-medium no-underline">
-                            Support
-                        </Link>
-                        <Link to="/corporate-sales" className="text-white font-medium no-underline">
-                            Corporate Sales
+        <nav className='lg:fixed left-0 right-0 bg-white z-[1000]'>
+            <Container>
+                <div className="bg-bg flex flex-col lg:flex-row flex-wrap justify-between items-center">
+                    <div className="logo">
+                        <Link to="/">
+                            <img className="h-24 w-40" src={logo} alt="logo" />
                         </Link>
                     </div>
 
-                    <div className="menu-2 bg-primaryBg py-3 h-5 pl-11 pr-5 rounded-[33px] flex items-center gap-4">
-                        <Search />
-                        <Link to={"/cart"}>
-                            <img src={cartIcon} alt="cart icon" />
-                        </Link>
-
-                        {/* Hamburger menu button for small devices */}
+                    <div className="menu flex flex-col md:flex-row gap-4 items-center">
                         <div
-                            className="md:hidden cursor-pointer"
-                            onClick={handleMenuToggle}
+                            className={`menu-1 bg-primary flex flex-col gap-4 py-3 px-8 rounded-[33px] ${menuOpen ? 'block' : 'hidden md:block'
+                                } md:flex md:flex-row`}
                         >
-                            <svg
-                                className="h-6 w-6 text-[#4D6067]"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
+                            <Link to="/shop" className="text-white font-medium no-underline">
+                                Shop
+                            </Link>
+                            <Link to="/technology" className="text-white font-medium no-underline">
+                                Technology
+                            </Link>
+                            <Link to="/support" className="text-white font-medium no-underline">
+                                Support
+                            </Link>
+                            <Link to="/corporate-sales" className="text-white font-medium no-underline">
+                                Corporate Sales
+                            </Link>
+                        </div>
+
+                        <div className="menu-2 bg-primaryBg py-3 h-5 pl-11 pr-5 rounded-[33px] flex items-center gap-4">
+                            <Search />
+                            <Link to={"/cart"}>
+                                <img src={cartIcon} alt="cart icon" />
+                            </Link>
+
+                            {/* Hamburger menu button for small devices */}
+                            <div
+                                className="md:hidden cursor-pointer"
+                                onClick={handleMenuToggle}
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16m-7 6h7"
-                                ></path>
-                            </svg>
+                                <svg
+                                    className="h-6 w-6 text-[#4D6067]"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16m-7 6h7"
+                                    ></path>
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </nav>
     );
 };
 
